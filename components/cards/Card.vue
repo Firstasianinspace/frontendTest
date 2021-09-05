ef3328960ad9ba7170ef2b60ef27175a4dde5f82
FirstAsianInSpace <55629177+Firstasianinspace@users.noreply.github.com>
2021-09-06 01:49:24 +0800

    auction

--- components/cards/Card.vue
+++ components/cards/Card.vue
@@ -24,15 +24,31 @@
       <div slot="error">Ошибка при загрузке изображения</div>
     </VueLoadImage>
     <div class="card-text">
-      <h4 class="card-text__title">{{card.name}}</h4>
-      <p class="card-text__desc">{{ card.description }}</p>
-      <p class="card-text__price">{{ card.price | currency }}</p>
+      <h4 class="card-text__title">
+        {{card.name}}
+      </h4>
+      <p class="card-text__price">
+        {{getHighestBid | currency }}
+      </p>
+      <p class="card-text__time">
+        <VueCountdown :time="getTime">
+          <template slot-scope="props">
+            {{ props.days }} {{getNoun(props.days, 'день', 'дня', 'дней')}},
+            {{ props.hours }} {{getNoun(props.hours, 'час', 'часа', 'часов')}},
+            {{ props.minutes }} {{getNoun(props.minutes, 'минута', 'минуты', 'минут')}}.
+          </template>
+        </VueCountdown>
+      </p>
+      <nuxt-link :to="`product/${card.id}`" class="card-text__link">
+        Перейти
+      </nuxt-link>
     </div>
   </div>
 </template>
 
 <script>
 import VueLoadImage from 'vue-load-image'
+import VueCountdown from '@chenfengyuan/vue-countdown'
 import Spinner from '@/components/Spinner'
 import CardDeleteIcon from '@/components/cards/CardDeleteIcon'
 
@@ -48,14 +64,45 @@ export default {
   components: {
     VueLoadImage,
     Spinner,
-    CardDeleteIcon
+    CardDeleteIcon,
+    VueCountdown
   },
   data () {
     return {
-      cardHover: false
+      cardHover: false,
+      time: null
+    }
+  },
+  computed: {
+    getHighestBid () {
+      const bids = this.card.bids
+      const filterBids = bids.map((value) => {
+        return parseInt(value.price, 10)
+      })
+      return Math.max(...filterBids)
+    },
+    getTime () {
+      const now = new Date()
+      const finished = this.card.ended_at
+      return Date.parse(finished) - Date.parse(now)
     }
   },
   methods: {
+    getNoun (number, one, two, five) {
+      let n = Math.abs(number)
+      n %= 100
+      if (n >= 5 && n <= 20) {
+        return five
+      }
+      n %= 10
+      if (n === 1) {
+        return one
+      }
+      if (n >= 2 && n <= 4) {
+        return two
+      }
+      return five
+    },
     // Not ideal
     removePost (id) {
       const singlePost = this.$refs.singleCard
@@ -73,30 +120,41 @@ export default {
     mouseLeave () {
       this.cardHover = false
     }
-    // Doesn't work
-    // enter (el) {
-    //   this.$gsap.to(el, {
-    //     opacity: 1,
-    //     duration: 1
-    //   })
-    //   console.log('enter')
-    // },
-    // leave (el) {
-    //   this.$gsap.to(el, {
-    //     opacity: 0,
-    //     duration: 1
-    //   })
-    //   console.log('leave')
-    // }
+  },
+  mounted () {
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .5s;
-}
-.fade-enter, .fade-leave-to {
-  opacity: 0;
+.card {
+  background: #FFFEFB;
+  box-shadow: 0px 20px 30px rgba(0, 0, 0, 0.04), 0px 6px 10px rgba(0, 0, 0, 0.02);
+  border-radius: 4px;
+  cursor: pointer;
+  &-image {
+    height: 250px;
+    background-position: center center;
+    background-size: cover;
+    position: relative;
+  }
+  &-text {
+    padding: 16px 16px 24px 16px;
+    background: #FAF8FF;
+    &__title {
+      font-size: 20px;
+      line-height: 25px;
+      color: #3F3F3F;
+    }
+    &__price {
+      font-weight: 600;
+      font-size: 24px;
+      line-height: 30px;
+      color: #3F3F3F;
+    }
+    &__link {
+
+    }
+  }
 }
 </style>
---
+++ pages/product/_id.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <div class="product-page">
+      <div class="product-gallery">
+        <img :src="product.imageLink" alt="">
+      </div>
+      <div class="product-desc">
+        {{product.name}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  data () {
+    return {
+      id: this.$route.params.id
+    }
+  },
+  computed: {
+    ...mapState('posts', ['list']),
+    /* eslint-disable */
+    // when develop API id=number and id=string
+    product () {
+      return this.list.find(el => el.id == this.id)
+    }
+  },
+  mounted () {
+    console.log(this.list)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.product-page {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
+</style>
